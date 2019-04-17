b16d9137abb3c9c2681acd5e04ef953c4d084409
Hoang Nguyen <nguyenvuhoang1994@gmail.com>
2019-04-17 21:24:12 +0200

    First Commit

---
+++ MI138-Project-ReviewMaster/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MI138_Project_ReviewMaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
